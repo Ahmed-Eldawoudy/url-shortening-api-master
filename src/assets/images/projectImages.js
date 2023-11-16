2ac65ab3d96b840b65a170fbe41cb38071550c5b
Ahmed El-dawoudy <60572865+Ahmed-Eldawoudy@users.noreply.github.com>
2023-11-16 13:25:08 +0200

    header&drawer

--- src/assets/images/projectImages.js
+++ src/assets/images/projectImages.js
@@ -1,3 +1,13 @@
 export const projectImages = {
   logo: new URL("./logo.svg", import.meta.url).href,
+  workingIllust: new URL("./illustration-working.svg", import.meta.url).href,
+  brandRecognition: new URL("./icon-brand-recognition.svg", import.meta.url),
+  detailedRecords: new URL("./icon-detailed-records.svg", import.meta.url).href,
+  fullyCustomizable: new URL("./icon-fully-customizable.svg", import.meta.url)
+    .href,
+  // Icons
+  twitter: new URL("./icon-twitter.svg", import.meta.url).href,
+  pinterest: new URL("./icon-pinterest.svg", import.meta.url).href,
+  instagram: new URL("./icon-instagram.svg", import.meta.url).href,
+  facebook: new URL("./icon-facebook.svg", import.meta.url).href,
 };
